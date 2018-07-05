8337c95db950148cef0cf932e9fce63e2e5e8658
Danilo Barros <danilobjr@gmail.com>
2018-07-05 11:49:57 -0300

    move gql to specific files

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { importSchema } = require('graphql-import');
 const data = require('./data');
 
-const typeDefs = `
-  type Person {
-    id: ID!
-    name: String!
-    email: String!
-    occupation: String
-    age: Int
-  }
-
-  type Query {
-    people: [Person]
-  }
-`;
+const typeDefs = importSchema('graphql/schema.graphql');
 
 const resolvers = {
-  Query: {
-    people: () => data.people,
-  },
+	Query: {
+		people: () => data.people
+	}
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
 const serverOptions = {
-  port: 9000,
+	port: 9000
 };
 
-server.start(
-  serverOptions,
-  ({ port }) => console.log(`Server is running on localhost: ${port}`),
-);
+server.start(serverOptions, ({ port }) => console.log(`Server is running on localhost: ${port}`));
