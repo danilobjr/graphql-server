d9c0338c8de9ccc2e5ea31f6a9cf69be81a9baf7
Danilo Barros <danilobjr@gmail.com>
2018-07-06 12:47:04 -0300

    createPerson mutation

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { importSchema } = require('graphql-import');
+const nanoid = require('nanoid');
 const data = require('./data');
 
 const typeDefs = importSchema('graphql/schema.graphql');
 
 const resolvers = {
-	Query: {
-		people: () => data.people,
-		person: (_, { name }) => data.people.find((p) => p.name.includes(name)),
-	},
+  Query: {
+    people: () => data.people,
+    person: (_, { name }) => data.people.find((p) => p.name.includes(name)),
+  },
+  Mutation: {
+    createPerson: (_, { person: newPerson }) => {
+      newPerson = {
+        id: nanoid(),
+        ...newPerson,
+      };
+
+      data.people = [...data.people, newPerson];
+
+      return newPerson;
+    },
+  }
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
 const serverOptions = {
-	port: 9000
+  port: 9000
 };
 
 server.start(
